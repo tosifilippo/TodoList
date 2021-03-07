785be88d7aabed5b8816b732232c5e865ea4067e
Filippo <tosifilippo@outlook.it>
2021-03-07 10:07:29 +0100

    fixed create task bug

--- src/modules/app.js
+++ src/modules/app.js
@@ -60,6 +60,10 @@ createTaskButton.addEventListener("click", function addTask() {
         saveLocal();
         addTaskForm.hidden = true;
         populatePage();
+        let node = document.querySelectorAll(`[data-index="${createTaskButton.dataset.index}"]`);
+        node.forEach(element => {
+            element.hidden = false;
+        })  
     };
 });
 // hides project form
@@ -174,7 +178,7 @@ function populatePage() {
                 } else {
                     taskDisplay.hidden = true;
                     showTasksButton.innerHTML = "Show Tasks";
-                }
+                };
             });
             showTasksButton.innerHTML = "Show Tasks";
             // makes sure hide/show button is only visible if the 
@@ -188,57 +192,57 @@ function populatePage() {
             // looping through each project and displaying its tasks     
             project.tasks.forEach(task => {
                 if (task != null) {
-                // saves task edit
-                saveTaskButton.addEventListener("click", function editTask() {
-                    projectsArray[saveTaskButton.dataset.projectindex].
-                    tasks[saveTaskButton.dataset.taskindex].title = editTaskTitle.value;
-                    projectsArray[saveTaskButton.dataset.projectindex].
-                    tasks[saveTaskButton.dataset.taskindex].description = editTaskDescription.value;
-                    projectsArray[saveTaskButton.dataset.projectindex].
-                    tasks[saveTaskButton.dataset.taskindex].dueDate = editTaskDueDate.value;
-                    projectsArray[saveTaskButton.dataset.projectindex].
-                    tasks[saveTaskButton.dataset.taskindex].priority = editTaskPriority.value;
-                    projectsArray[saveTaskButton.dataset.projectindex].
-                    tasks[saveTaskButton.dataset.taskindex].notes = editTaskNotes.value;
-                    saveLocal();
-                    editTaskForm.hidden = true;
-                    populateTask();        
-                });
-                let taskPara = document.createElement("p");
-                let deleteTaskButton = document.createElement("button");
-                deleteTaskButton.innerHTML = "Delete Task";
-                let editTaskButton = document.createElement("button");
-                editTaskButton.innerHTML = "Edit Task";
-                function populateTask() {
-                    taskPara.innerHTML = "<br>Task: " + task.title + "<br>" +
-                    "Description: " + task.description  + "<br>" + 
-                    "Due Date: " + task.dueDate  + "<br>" +
-                    "Priority: " + task.priority + "<br>" + 
-                    "Notes: " + task.notes + "<br>";
-                    // appending elements to their parents
-                    taskPara.append(editTaskButton, deleteTaskButton);
-                    taskDisplay.append(taskPara);
-                }
-                populateTask();
-                deleteTaskButton.setAttribute("data-index", project.tasks.indexOf(task));
-                // delete task button listener
-                deleteTaskButton.addEventListener("click", function() {
-                    delete project.tasks[deleteTaskButton.dataset.index];
-                    checkTasks();
-                    saveLocal();
-                    taskDisplay.removeChild(taskPara);
-                });
-                // edit task button listener
-                editTaskButton.addEventListener("click", function() {
-                    editTaskForm.hidden = false;
-                    saveTaskButton.setAttribute("data-projectindex", projectsArray.indexOf(project));
-                    saveTaskButton.setAttribute("data-taskindex", project.tasks.indexOf(task));
-                    editTaskTitle.value = task.title;
-                    editTaskDescription.value = task.description;
-                    editTaskDueDate.value = task.dueDate;
-                    editTaskPriority.value = task.priority;
-                    editTaskNotes.value = task.notes; 
-                })
+                    // saves task edit
+                    saveTaskButton.addEventListener("click", function editTask() {
+                        projectsArray[saveTaskButton.dataset.projectindex].
+                        tasks[saveTaskButton.dataset.taskindex].title = editTaskTitle.value;
+                        projectsArray[saveTaskButton.dataset.projectindex].
+                        tasks[saveTaskButton.dataset.taskindex].description = editTaskDescription.value;
+                        projectsArray[saveTaskButton.dataset.projectindex].
+                        tasks[saveTaskButton.dataset.taskindex].dueDate = editTaskDueDate.value;
+                        projectsArray[saveTaskButton.dataset.projectindex].
+                        tasks[saveTaskButton.dataset.taskindex].priority = editTaskPriority.value;
+                        projectsArray[saveTaskButton.dataset.projectindex].
+                        tasks[saveTaskButton.dataset.taskindex].notes = editTaskNotes.value;
+                        saveLocal();
+                        editTaskForm.hidden = true;
+                        populateTask();        
+                    });
+                    let taskPara = document.createElement("p");
+                    let deleteTaskButton = document.createElement("button");
+                    deleteTaskButton.innerHTML = "Delete Task";
+                    let editTaskButton = document.createElement("button");
+                    editTaskButton.innerHTML = "Edit Task";
+                    function populateTask() {
+                        taskPara.innerHTML = "<br>Task: " + task.title + "<br>" +
+                        "Description: " + task.description  + "<br>" + 
+                        "Due Date: " + task.dueDate  + "<br>" +
+                        "Priority: " + task.priority + "<br>" + 
+                        "Notes: " + task.notes + "<br>";
+                        // appending elements to their parents
+                        taskPara.append(editTaskButton, deleteTaskButton);
+                        taskDisplay.append(taskPara);
+                    }
+                    populateTask();
+                    deleteTaskButton.setAttribute("data-index", project.tasks.indexOf(task));
+                    // delete task button listener
+                    deleteTaskButton.addEventListener("click", function() {
+                        delete project.tasks[deleteTaskButton.dataset.index];
+                        checkTasks();
+                        saveLocal();
+                        taskDisplay.removeChild(taskPara);
+                    });
+                    // edit task button listener
+                    editTaskButton.addEventListener("click", function() {
+                        editTaskForm.hidden = false;
+                        saveTaskButton.setAttribute("data-projectindex", projectsArray.indexOf(project));
+                        saveTaskButton.setAttribute("data-taskindex", project.tasks.indexOf(task));
+                        editTaskTitle.value = task.title;
+                        editTaskDescription.value = task.description;
+                        editTaskDueDate.value = task.dueDate;
+                        editTaskPriority.value = task.priority;
+                        editTaskNotes.value = task.notes; 
+                    });
                 };
             });
             projectDisplay.append(projectPara, taskDisplay, showTasksButton,
